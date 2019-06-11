d803ed70247db1caa8e11431b790a01cb4dc6fd2
kgskgs <kgs.void@gmail.com>
2019-06-11 19:57:16 +0300

    updated comments & test run

--- linear_discriminant.py
+++ linear_discriminant.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
@@ -5,6 +6,7 @@ from math import sqrt
 
 def get_loss(y, y_hat, w, X, b):
     """
+    loss function for two classes
     :param y: value
     :param y_hat: predicted value
     :param w: weights
@@ -19,6 +21,7 @@ def get_loss(y, y_hat, w, X, b):
 
 def predict(w, b, x):
     """
+    prediction for two classes
     :param w: weights
     :param b: biases
     :param x: observation
@@ -30,6 +33,7 @@ def predict(w, b, x):
 
 def normalize_v(v):
     """
+    normalize a vector
     :param v: vector
     :return: normalized v
     """
@@ -42,6 +46,17 @@ d = np.loadtxt(open("./data/linear_datat_2class.csv", "r", encoding='utf-8-sig')
 
 
 def minimize_loss(eta_w, eta_b, w, b, X, y, it = 100):
+    """
+    minimize the loss function using gradient / partial derivates with respect to w and b
+    :param eta_w: learning rate weights
+    :param eta_b: learning rate biase ?
+    :param w: initial weight
+    :param b: initial bias
+    :param X: training data
+    :param y: training labels
+    :param it: # iterations to optimize 
+    :returns: final weights and bias
+    """
     for z in range(it):
         w = normalize_v(w)
         delta_w = np.array([0.0, 0.0])
@@ -56,34 +71,35 @@ def minimize_loss(eta_w, eta_b, w, b, X, y, it = 100):
 
         w = w - eta_w*normalize_v(delta_w)
         b = b - eta_b*delta_b
-        print(z, w, b, delta_w, delta_b, get_loss(y, y_hat, w, X, b))
+        #print(z, w, b, delta_w, delta_b, get_loss(y, y_hat, w, X, b))
     return (normalize_v(w), b)
 
-
-X, y = d[:,:-1], d[:,-1:]
-colors = ["red" if x==1 else "blue" for x in y]
-
-
-plt.scatter(X[:,0], X[:,1], color=colors)
+if __name__=="__main__":
+    X, y = d[:,:-1], d[:,-1:]
+    colors = ["red" if x==1 else "blue" for x in y]
 
 
-w, b = minimize_loss(0.03, 0.03, np.array([0.5, 0.5]), 1, X, y)
+    plt.scatter(X[:,0], X[:,1], color=colors)
 
 
-point1 = [0, -b/w[0]]
-point2 = [-b/w[1], 0]
+    w, b = minimize_loss(0.03, 0.03, np.array([0.5, 0.5]), 1, X, y)
 
 
-plt.axis('equal')
-#plt.axis([30, 100, 30, 100])
-plt.plot(point1, point2, '-r', color="green")
+    point1 = [0, -b/w[0]]
+    point2 = [-b/w[1], 0]
 
 
-correct = 0
-predictions = []
-for i in range(len(y)):
-    predictions.append(predict(w, b, X[i]))
-    if predict(w, b, X[i]) == y[i]:
-        correct += 1
+    plt.axis('equal')
+    #plt.axis([30, 100, 30, 100])
+    plt.plot(point1, point2, '-r', color="green")
 
-print("{}/100".format(correct))
+
+    correct = 0
+    predictions = []
+    for i in range(len(y)):
+        predictions.append(predict(w, b, X[i]))
+        if predict(w, b, X[i]) == y[i]:
+            correct += 1
+
+    print("{}/100".format(correct))
+    plt.show()
