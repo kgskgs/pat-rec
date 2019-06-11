c2eb1886272a0d4239a1e115c91dfdad2f1aecb1
Stoyanov <Kalin.Stoyanov-BOR@lightspeedresearch.com>
2019-06-11 19:27:41 +0300

    finished linear discriminant perceptron

--- linear_discriminant.py
+++ linear_discriminant.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+from math import sqrt
 
 
 def get_loss(y, y_hat, w, X, b):
@@ -13,15 +14,76 @@ def get_loss(y, y_hat, w, X, b):
     """
     loss = 0
     for i in range(len(y)):
-        loss += (y_hat[i] - y[i]) * \
-            (w * X[i] + b) / 2
+        loss += (y_hat[i] - y[i]) * (w @ X[i] + b) / 2
+    return loss
 
+def predict(w, b, x):
+    """
+    :param w: weights
+    :param b: biases
+    :param x: observation
+    :return: +1 if the observation is above the boundry -1 otherwise
+    """
+    res = w @ x + b
+    if  res > 0: return 1
+    return -1
 
+def normalize_v(v):
+    """
+    :param v: vector
+    :return: normalized v
+    """
+    norm = sqrt(sum([x*x for x in v]))
+    if norm == 0:
+        raise ValueError("cannot normalize zero length vector")
+    return np.array([x/norm for x in v])
 
 d = np.loadtxt(open("./data/linear_datat_2class.csv", "r", encoding='utf-8-sig'), delimiter=",", skiprows=0)
 
 
+def minimize_loss(eta_w, eta_b, w, b, X, y, it = 100):
+    for z in range(it):
+        w = normalize_v(w)
+        delta_w = np.array([0.0, 0.0])
+        delta_b = 0.0
+        y_hat = []
+        for i in range(len(y)):
+            prediction = (predict(w, b, X[i]) - y[i])
+            y_hat.append(prediction)
+            delta_w += prediction * X[i] / 2
+            delta_b += prediction / 2
+
+
+        w = w - eta_w*normalize_v(delta_w)
+        b = b - eta_b*delta_b
+        print(z, w, b, delta_w, delta_b, get_loss(y, y_hat, w, X, b))
+    return (normalize_v(w), b)
+
 
 X, y = d[:,:-1], d[:,-1:]
+colors = ["red" if x==1 else "blue" for x in y]
 
-print(X)
+
+plt.scatter(X[:,0], X[:,1], color=colors)
+
+
+w, b = minimize_loss(0.03, 0.03, np.array([0.5, 0.5]), 1, X, y)
+
+
+point1 = [0, -b/w[0]]
+point2 = [-b/w[1], 0]
+
+
+plt.axis('equal')
+#plt.axis([30, 100, 30, 100])
+plt.plot(point1, point2, '-r', color="green")
+
+
+correct = 0
+predictions = []
+for i in range(len(y)):
+    predictions.append(predict(w, b, X[i]))
+    if predict(w, b, X[i]) == y[i]:
+        correct += 1
+
+print("{}/100".format(correct))
