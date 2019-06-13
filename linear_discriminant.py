e4e419aa374de57f2ac512efed0f6340340defb9
Kalin Stoyanov <Kalin.Stoyanov-BOR@lightspeedresearch.com>
2019-06-13 19:57:15 +0300

    turned linear discriminant script into class

--- linear_discriminant.py
+++ linear_discriminant.py
@@ -4,32 +4,64 @@ import matplotlib.pyplot as plt
 from math import sqrt
 
 
-def get_loss(y, y_hat, w, X, b):
-    """
-    loss function for two classes
-    :param y: value
-    :param y_hat: predicted value
-    :param w: weights
-    :param X: parameters of training data
-    :param b: bias ?
-    :return: :type float: loss
-    """
-    loss = 0
-    for i in range(len(y)):
-        loss += (y_hat[i] - y[i]) * (w @ X[i] + b) / 2
-    return loss
+class perceptron:
 
-def predict(w, b, x):
-    """
-    prediction for two classes
-    :param w: weights
-    :param b: biases
-    :param x: observation
-    :return: +1 if the observation is above the boundry -1 otherwise
-    """
-    res = w @ x + b
-    if res > 0: return 1
-    return -1
+    def __init__(self):
+        #self.w = np.random.rand(2)
+        self.w = np.array([0.5, 0.5])
+        self.w = normalize_v(self.w)
+        #self.b = np.random.rand()
+        self.b = 1
+
+
+    def get_loss(self, y, y_hat, X):
+        """
+        loss function for two classes
+        :param y: value
+        :param y_hat: predicted value
+        :param X: parameters of training data
+        :return: :type float: loss
+        """
+        loss = 0
+        for i in range(len(y)):
+            loss += (y_hat[i] - y[i]) * (self.w @ X[i] + self.b) / 2
+        return loss
+
+    def predict(self, x):
+        """
+        prediction for two classes
+        :param x: observation
+        :return: +1 if the observation is above the boundry -1 otherwise
+        """
+        res = self.w @ x + self.b
+        if res > 0: return 1
+        return -1
+
+    def minimize_loss(self, eta_w, eta_b, X, y, it = 100):
+        """
+        minimize the loss function using gradient / partial derivates with respect to w and b
+        :param eta_w: learning rate weights
+        :param eta_b: learning rate biase ?
+        :param X: training data
+        :param y: training labels
+        :param it: # iterations to optimize
+        :returns: final weights and bias
+        """
+        for z in range(it):
+            delta_w = np.array([0.0, 0.0])
+            delta_b = 0.0
+            y_hat = []
+            for i in range(len(y)):
+                prediction = (self.predict(X[i]) - y[i])
+                y_hat.append(prediction)
+                delta_w += prediction * X[i] / 2
+                delta_b += prediction / 2
+
+            self.w = normalize_v(self.w - eta_w*normalize_v(delta_w))
+            self.b = self.b - eta_b*delta_b
+            #print(z, self.w, self.b, delta_w, delta_b, self.get_loss(y, y_hat, X))
+        return (self.w, self.b)
+
 
 def normalize_v(v):
     """
@@ -42,35 +74,6 @@ def normalize_v(v):
         raise ValueError("cannot normalize zero length vector")
     return np.array([x/norm for x in v])
 
-
-def minimize_loss(eta_w, eta_b, w, b, X, y, it = 100):
-    """
-    minimize the loss function using gradient / partial derivates with respect to w and b
-    :param eta_w: learning rate weights
-    :param eta_b: learning rate biase ?
-    :param w: initial weight
-    :param b: initial bias
-    :param X: training data
-    :param y: training labels
-    :param it: # iterations to optimize 
-    :returns: final weights and bias
-    """
-    for z in range(it):
-        w = normalize_v(w)
-        delta_w = np.array([0.0, 0.0])
-        delta_b = 0.0
-        y_hat = []
-        for i in range(len(y)):
-            prediction = (predict(w, b, X[i]) - y[i])
-            y_hat.append(prediction)
-            delta_w += prediction * X[i] / 2
-            delta_b += prediction / 2
-
-        w = w - eta_w*normalize_v(delta_w)
-        b = b - eta_b*delta_b
-        #print(z, w, b, delta_w, delta_b, get_loss(y, y_hat, w, X, b))
-    return (normalize_v(w), b)
-
 if __name__=="__main__":
     d = np.loadtxt(open("./data/linear_datat_2class.csv", "r", encoding='utf-8-sig'), delimiter=",", skiprows=0)
 
@@ -79,7 +82,9 @@ if __name__=="__main__":
 
     plt.scatter(X[:,0], X[:,1], color=colors)
 
-    w, b = minimize_loss(0.03, 0.03, np.array([0.5, 0.5]), 1, X, y)
+    perc = perceptron()
+
+    w, b = perc.minimize_loss(0.3, 0.3, X, y, 100)
 
     point1 = [0, -b/w[0]]
     point2 = [-b/w[1], 0]
@@ -88,12 +93,11 @@ if __name__=="__main__":
     #plt.axis([30, 100, 30, 100])
     plt.plot(point1, point2, '-r', color="green")
 
-
     correct = 0
     predictions = []
     for i in range(len(y)):
-        predictions.append(predict(w, b, X[i]))
-        if predict(w, b, X[i]) == y[i]:
+        predictions.append(perc.predict(X[i]))
+        if perc.predict(X[i]) == y[i]:
             correct += 1
 
     print("{}/100".format(correct))
